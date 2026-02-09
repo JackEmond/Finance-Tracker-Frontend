c5f83f4c3cb04bf06005a2211335bbbf1a14960e
Jack Emond <jackemond@outlook.com>
2026-02-09 00:02:27 -0500

    summary data now shown

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,24 @@ interface Transaction {
   id: number;
   description: string;
   amount: number;
+  category_name: string;
 }
 
 function App() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const positiveTransactions = transactions.filter((t) => t.amount > 0);
+  const negativeTransactions = transactions.filter((t) => t.amount < 0);
+  const totalIncome = positiveTransactions.reduce(
+    (sum, t) => sum + Number(t.amount),
+    0,
+  );
+  console.log(totalIncome);
+
+  const totalExpenses = negativeTransactions.reduce(
+    (sum, t) => sum + Number(t.amount),
+    0,
+  );
+  const netBalance = Number(totalIncome) + Number(totalExpenses);
 
   useEffect(() => {
     fetch("http://localhost:5000/transactions")
@@ -28,7 +42,7 @@ function App() {
       <div className="summary-container">
         <div className="stat-card full-width">
           <div className="stat-label center-text">Net Balance</div>
-          <div className="net-balance-value">$0.00</div>
+          <div className="net-balance-value">${netBalance}</div>
           <div className="balance-change">
             <div className="prev-amount">
               <b className="white">Prev:</b> 0.00
@@ -40,7 +54,7 @@ function App() {
         {/* Total Income Card */}
         <div className="stat-card">
           <div className="stat-label">Total Income</div>
-          <div className="stat-value green">$0.00</div>
+          <div className="stat-value green">${totalIncome}</div>
           <div className="balance-change">
             <div className="prev-amount">
               <b className="white">Prev:</b> 0.00
@@ -52,7 +66,7 @@ function App() {
         {/* Total Expenses Card */}
         <div className="stat-card">
           <div className="stat-label">Total Expenses</div>
-          <div className="stat-value red">$0.00</div>
+          <div className="stat-value red">${Math.abs(totalExpenses)}</div>
           <div className="balance-change">
             <div className="prev-amount">
               <b className="white">Prev:</b> 0.00
@@ -65,18 +79,24 @@ function App() {
         <div className="stat-card full-width">
           <div className="stat-label center-text">Expense Breakdown</div>
           <table className="table-card">
-            <tr>
-              <th>Category</th>
-              <th>Previous Month</th>
-              <th>This Month</th>
-              <th>Difference</th>
-            </tr>
-            <tr>
-              <td>category name</td>
-              <td>$0.00</td>
-              <td>$0.00</td>
-              <td>0.0%</td>
-            </tr>
+            <thead>
+              <tr>
+                <th>Category</th>
+                <th>Previous Month</th>
+                <th>This Month</th>
+                <th>Difference</th>
+              </tr>
+            </thead>
+            <tbody>
+              {negativeTransactions.map((transaction) => (
+                <tr key={transaction.id}>
+                  <td>{transaction.category_name}</td>
+                  <td>$0.00</td>
+                  <td>${transaction.amount}</td>
+                  <td>0.0%</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
 
@@ -84,18 +104,24 @@ function App() {
         <div className="stat-card full-width">
           <div className="stat-label center-text">Income Breakdown</div>
           <table className="table-card">
-            <tr>
-              <th>Category</th>
-              <th>Previous Month</th>
-              <th>This Month</th>
-              <th>Difference</th>
-            </tr>
-            <tr>
-              <td>category name</td>
-              <td>$0.00</td>
-              <td>$0.00</td>
-              <td>0.0%</td>
-            </tr>
+            <thead>
+              <tr>
+                <th>Category</th>
+                <th>Previous Month</th>
+                <th>This Month</th>
+                <th>Difference</th>
+              </tr>
+            </thead>
+            <tbody>
+              {positiveTransactions.map((transaction) => (
+                <tr key={transaction.id}>
+                  <td>{transaction.category_name}</td>
+                  <td>$0.00</td>
+                  <td>${transaction.amount}</td>
+                  <td>0.0%</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
 
@@ -103,21 +129,25 @@ function App() {
         <div className="stat-card full-width">
           <div className="stat-label center-text">Recent Transactions</div>
           <table className="table-card">
-            <tr>
-              <th>Description</th>
-              <th>Amount</th>
-              <th>Type</th>
-            </tr>
-            {transactions.map((transaction) => (
-              <tr key={transaction.id}>
-                <td>{transaction.description}</td>
-                <td className={transaction.amount < 0 ? "red" : "green"}>
-                  {transaction.amount < 0 ? "-" : "+"}$
-                  {Math.abs(transaction.amount).toFixed(2)}
-                </td>
-                <td>{transaction.amount < 0 ? "Expense" : "Income"}</td>
+            <thead>
+              <tr>
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Category</th>
               </tr>
-            ))}
+            </thead>
+            <tbody>
+              {transactions.map((transaction) => (
+                <tr key={transaction.id}>
+                  <td>{transaction.description}</td>
+                  <td className={transaction.amount < 0 ? "red" : "green"}>
+                    {transaction.amount < 0 ? "-" : "+"}$
+                    {Math.abs(transaction.amount)}
+                  </td>
+                  <td>{transaction.category_name}</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
